61af1917ae68d8c3c4c59d2f5cca45c0ab3c159d
sudhakar-se <sudhakar@sustainabilityeconomics.com>
2024-05-22 00:59:36 +0530

    next

---
+++ src/app/image/page.tsx
@@ -0,0 +1,14 @@
+// 'use client'
+import React from 'react'
+import Dummy from '@/assets/dummy.jpg'
+import Image from 'next/image'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (<Image alt ="gg"src={Dummy}/>
+    
+  )
+}
+
+export default page
--- src/service/store.js
+++
@@ -1,9 +0,0 @@
-// src/app/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import pokemonReducer from './pokemon';
-
-export const store = configureStore({
-  reducer: {
-    pokemon: pokemonReducer,
-  },
-});
