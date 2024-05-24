78d7eda616c10b9012632147aed3b2444ce6925f
sudhakar-se <sudhakar@sustainabilityeconomics.com>
2024-05-25 03:11:39 +0530

    scam

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Pokemon } from "./component/Pokemon1";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const a = {
   hi: "j",
   j: "d",
@@ -18,21 +18,18 @@ const pokemon = [
 export default function App() {
   const [pollingInterval, setPollingInterval] = useState(0);
 
+  useEffect(()=>{
+    try{
+      
+    }catch(e){}
+  })
+
   return (
+
+
     <div className="App">
-      <select
-        onChange={(change) => setPollingInterval(Number(change.target.value))}
-      >
-           
-             <option value={0}>Off</option>
-        <option value={1000}>1s</option>
-        <option value={5000}>5s</option>
-      </select>
-      <div>
-        {pokemon.map((poke, index) => (
-          <Pokemon key={index} name={poke} pollingInterval={pollingInterval} />
-        ))}
-      </div>
+    <h1 style={{color:'red',textAlign:'center'}}>I am a scammer site</h1>
+
     </div>
   );
 }
