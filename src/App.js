28b68ce9dd5a6a97718270d643095fc7f88a6c5e
sudhakar-se <sudhakar@sustainabilityeconomics.com>
2024-05-28 20:38:31 +0530

    falulty commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import { Pokemon } from "./component/Pokemon1";
-import { useEffect, useState } from "react";
-const a = {
-  hi: "j",
-  j: "d",
-  j: "d",
-};
+// src/components/HazardMap.jsx
+import React from "react";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import hazardData from "./data/hazardData.json"; // Adjust the path according to your project structure
 
-const pokemon = [
-  "bulbasaur",
-  "pikachu",
-  "ditto",
-  "bulbasaur",
-  "bulbasaur",
-  "bulbasaur",
-];
-
-export default function App() {
-  const [pollingInterval, setPollingInterval] = useState(0);
-
-  useEffect(() => {
-    try {
-    } catch (e) {}
+const HazardMap = () => {
+  const getColor = (temperature) => {
+    return temperature > 300
+      ? "#800026"
+      : temperature > 290
+        ? "#BD0026"
+        : temperature > 280
+          ? "#E31A1C"
+          : temperature > 270
+            ? "#FC4E2A"
+            : temperature > 260
+              ? "#FD8D3C"
+              : temperature > 250
+                ? "#FEB24C"
+                : temperature > 240
+                  ? "#FED976"
+                  : "#FFEDA0";
+  };
+  a;
+  const style = (feature) => ({
+    fillColor: getColor(feature.properties.temperature),
+    weight: 1,
+    opacity: 1,
+    color: "blue",
+    fillOpacity: 0.7,
   });
 
   return (
-    <div className="App">
-      <h1 style={{ color: "red", textAlign: "center" }}>I am a scammer site</h1>
-    </div>
+    <>
+      <h1>Hi this me</h1>
+      <MapContainer
+        center={[0, 0]}
+        zoom={2}
+        style={{ height: "600px", width: "800px" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href='https://osm.org/copyright'>OpenStreetMap</a> contributors"
+        />
+        <GeoJSON data={hazardData} style={style} />
+      </MapContainer>
+    </>
   );
-}
+};
+
+export default HazardMap;
---
+++ src/json.js
@@ -0,0 +1,185 @@
+export const dummyGeoJSON = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [0.9375, 0],
+      },
+      properties: {
+        temperature: 257.4,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [2.8125, 0],
+      },
+      properties: {
+        temperature: 257.3,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [4.6875, 0],
+      },
+      properties: {
+        temperature: 257.4,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [6.5625, 0],
+      },
+      properties: {
+        temperature: 257.5,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [8.4375, 0],
+      },
+      properties: {
+        temperature: 257.3,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [10.3125, 0],
+      },
+      properties: {
+        temperature: 257.2,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [12.1875, 0],
+      },
+      properties: {
+        temperature: 257.1,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [14.0625, 0],
+      },
+      properties: {
+        temperature: 257.3,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [15.9375, 0],
+      },
+      properties: {
+        temperature: 257.4,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [17.8125, 0],
+      },
+      properties: {
+        temperature: 257.5,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [19.6875, 0],
+      },
+      properties: {
+        temperature: 257.3,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [21.5625, 0],
+      },
+      properties: {
+        temperature: 257.2,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [23.4375, 0],
+      },
+      properties: {
+        temperature: 257.1,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [25.3125, 0],
+      },
+      properties: {
+        temperature: 257.4,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [27.1875, 0],
+      },
+      properties: {
+        temperature: 257.5,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [29.0625, 0],
+      },
+      properties: {
+        temperature: 257.3,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [30.9375, 0],
+      },
+      properties: {
+        temperature: 257.2,
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [32.8125, 0],
+      },
+      properties: {
+        temperature: 257.1,
+      },
+    },
+  ],
+};
